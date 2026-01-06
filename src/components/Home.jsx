c6ea57b8d7ab5348af355596b409a4f79c3667a7
AbiMinhaj <abiminhaj167@gmail.com>
2026-01-06 11:05:13 +0530

    added

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="flex min-h-screen bg-black text-white">
+        <div className="m-auto text-center">
+            
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
