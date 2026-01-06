2e073f9fe91cb0c17d5d4704c6c33547c45e6cfb
AbiMinhaj <abiminhaj167@gmail.com>
2026-01-06 12:51:09 +0530

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen bg-black text-white">
+      <aside className="w-64 border-r border-gray-800 px-6 py-8 hidden md:flex flex-col">
+        
+      </aside>
+
+    </div>
+  );
+}
