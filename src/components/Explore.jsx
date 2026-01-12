c4297f55761e89eac7793f84ae9050d3bc8d0712
thasnimpk <fathimathasnim220@gmail.com>
2026-01-12 17:27:53 +0530

    changed1

---
+++ src/components/Explore.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Explore() {
+  return (
+    <div className="h-screen w-full bg-black  flex play-regular ">
+
+
+
+
+    </div>
+  )
+}
+
+export default Explore
