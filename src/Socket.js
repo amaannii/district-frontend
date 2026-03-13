cdfbee2063f5a7e8fd4a9c7f83e48c539ad1a3dd
amaannii <amaaanniii@gmail.com>
2026-03-13 14:12:45 +0530

    socket api

--- src/Socket.js
+++ src/Socket.js
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+// const socket = io("http://localhost:3001", {
+//   withCredentials: true,
+// });
+const socket = io("https://district-backend-mjx0.onrender.com", {
   withCredentials: true,
 });
 
