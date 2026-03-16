c73ea7e92bed9b192f60f2a13501470bc51c6aab
amaannii <amaaanniii@gmail.com>
2026-03-16 12:57:01 +0530

    request errors

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:3001"
-});
 // const API = axios.create({
-//   baseURL: "https://district-backend-mjx0.onrender.com"
+//   baseURL: "http://localhost:3001"
 // });
+const API = axios.create({
+  baseURL: "https://district-backend-mjx0.onrender.com"
+});
 
 export default API; 
--- src/Socket.js
+++ src/Socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
-  withCredentials: true,
-});
-// const socket = io("https://district-backend-mjx0.onrender.com", {
+// const socket = io("http://localhost:3001", {
 //   withCredentials: true,
 // });
+const socket = io("https://district-backend-mjx0.onrender.com", {
+  withCredentials: true,
+});
 
 export default socket;
